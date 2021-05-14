087221e47e8ebc22a2cc240975c52db800377eb3
Mathew Chanda <mvchanda@ncsu.edu>
2021-05-14 14:09:06 -0400

    added phone model

---
+++ iSee/Model/Phone.swift
@@ -0,0 +1,15 @@
+//
+//  Phone.swift
+//  iSee
+//
+//  Created by Mathew Chanda on 5/14/21.
+//
+
+import Foundation
+class Phone {
+    var name : String?
+    var price : Int?
+    var description : String?
+    var company : String?
+    var features : [String]?
+}
